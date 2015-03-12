b6983de686d31b94ea6f6b8b2c883b46a3a11fda
Mat Brown <mat@genius.com>
2015-03-12 11:05:29 -0400

    Back to sh

--- script/build_ree.rb
+++ script/build_ree.rb
@@ -1,4 +1,9 @@
 require 'tmpdir'
+require 'fileutils'
+
+def sh(*args, &block)
+  FileUtils.sh(*args, &block)
+end
 
 def build_ree_command(name, output, prefix, usr_dir, tmpdir, rubygems = nil)
   build_command = [
@@ -10,7 +15,7 @@ def build_ree_command(name, output, prefix, usr_dir, tmpdir, rubygems = nil)
   build_command << "mv #{prefix} /app/vendor/#{output}" if prefix != "/app/vendor/#{output}"
   build_command = build_command.join(" && ")
 
-  system build_command
+  sh build_command
 
   #sh "vulcan build -v -o #{output}.tgz --prefix #{vulcan_prefix} --source #{name} --command=\"#{build_command}\""
   #s3_upload(tmpdir, output)
@@ -28,20 +33,20 @@ Dir.mktmpdir("ruby-") do |tmpdir|
   Dir.chdir(tmpdir) do |dir|
     FileUtils.rm_rf("#{tmpdir}/*")
 
-    system "curl https://rubyenterpriseedition.googlecode.com/files/#{full_name}.tar.gz -s -o - | tar zxf -"
+    sh "curl https://rubyenterpriseedition.googlecode.com/files/#{full_name}.tar.gz -s -o - | tar zxf -"
 
     Dir.chdir("#{full_name}/source") do |source_dir|
-      system "curl -L -o 34ba44f94a62c63ddf02a045b6f4edcd6eab4989.patch https://github.com/RapGenius/rubyenterpriseedition187-330/commit/34ba44f94a62c63ddf02a045b6f4edcd6eab4989.patch"
-      system "curl -L -o 5384967a015be227e16af7a332a50d45e14ed0ad.patch https://github.com/RapGenius/rubyenterpriseedition187-330/commit/5384967a015be227e16af7a332a50d45e14ed0ad.patch"
-      system "curl -L -o tcmalloc.patch https://raw.githubusercontent.com/wayneeseguin/rvm/master/patches/ree/1.8.7/tcmalloc.patch"
-      system "patch -p1 <34ba44f94a62c63ddf02a045b6f4edcd6eab4989.patch"
-      system "patch -p1 <5384967a015be227e16af7a332a50d45e14ed0ad.patch"
-      system "patch -p1 <tcmalloc.patch"
+      sh "curl -L -o 34ba44f94a62c63ddf02a045b6f4edcd6eab4989.patch https://github.com/RapGenius/rubyenterpriseedition187-330/commit/34ba44f94a62c63ddf02a045b6f4edcd6eab4989.patch"
+      sh "curl -L -o 5384967a015be227e16af7a332a50d45e14ed0ad.patch https://github.com/RapGenius/rubyenterpriseedition187-330/commit/5384967a015be227e16af7a332a50d45e14ed0ad.patch"
+      sh "curl -L -o tcmalloc.patch https://raw.githubusercontent.com/wayneeseguin/rvm/master/patches/ree/1.8.7/tcmalloc.patch"
+      sh "patch -p1 <34ba44f94a62c63ddf02a045b6f4edcd6eab4989.patch"
+      sh "patch -p1 <5384967a015be227e16af7a332a50d45e14ed0ad.patch"
+      sh "patch -p1 <tcmalloc.patch"
     end
 
     FileUtils.mkdir_p("#{full_name}/#{usr_dir}")
     Dir.chdir("#{full_name}/#{usr_dir}") do
-      system "curl http://production.cf.rubygems.org/rubygems/rubygems-#{rubygems}.tgz -s -o - | tar xzf -" if major_ruby == "1.8"
+      sh "curl http://production.cf.rubygems.org/rubygems/rubygems-#{rubygems}.tgz -s -o - | tar xzf -" if major_ruby == "1.8"
     end
 
     # runtime ruby
