84fc434cfbe222f1db787ea8e298267a396b5cac
Mat Brown <mat@genius.com>
2015-03-12 10:52:44 -0400

    system

--- script/build_ree.rb
+++ script/build_ree.rb
@@ -26,20 +26,20 @@ Dir.mktmpdir("ruby-") do |tmpdir|
   Dir.chdir(tmpdir) do |dir|
     FileUtils.rm_rf("#{tmpdir}/*")
 
-    sh "curl https://rubyenterpriseedition.googlecode.com/files/#{full_name}.tar.gz -s -o - | tar zxf -"
+    system "curl https://rubyenterpriseedition.googlecode.com/files/#{full_name}.tar.gz -s -o - | tar zxf -"
 
     Dir.chdir("#{full_name}/source") do |source_dir|
-      sh "curl -L -o 34ba44f94a62c63ddf02a045b6f4edcd6eab4989.patch https://github.com/RapGenius/rubyenterpriseedition187-330/commit/34ba44f94a62c63ddf02a045b6f4edcd6eab4989.patch"
-      sh "curl -L -o 5384967a015be227e16af7a332a50d45e14ed0ad.patch https://github.com/RapGenius/rubyenterpriseedition187-330/commit/5384967a015be227e16af7a332a50d45e14ed0ad.patch"
-      sh "curl -L -o tcmalloc.patch https://raw.githubusercontent.com/wayneeseguin/rvm/master/patches/ree/1.8.7/tcmalloc.patch"
-      sh "patch -p1 <34ba44f94a62c63ddf02a045b6f4edcd6eab4989.patch"
-      sh "patch -p1 <5384967a015be227e16af7a332a50d45e14ed0ad.patch"
-      sh "patch -p1 <tcmalloc.patch"
+      system "curl -L -o 34ba44f94a62c63ddf02a045b6f4edcd6eab4989.patch https://github.com/RapGenius/rubyenterpriseedition187-330/commit/34ba44f94a62c63ddf02a045b6f4edcd6eab4989.patch"
+      system "curl -L -o 5384967a015be227e16af7a332a50d45e14ed0ad.patch https://github.com/RapGenius/rubyenterpriseedition187-330/commit/5384967a015be227e16af7a332a50d45e14ed0ad.patch"
+      system "curl -L -o tcmalloc.patch https://raw.githubusercontent.com/wayneeseguin/rvm/master/patches/ree/1.8.7/tcmalloc.patch"
+      system "patch -p1 <34ba44f94a62c63ddf02a045b6f4edcd6eab4989.patch"
+      system "patch -p1 <5384967a015be227e16af7a332a50d45e14ed0ad.patch"
+      system "patch -p1 <tcmalloc.patch"
     end
 
     FileUtils.mkdir_p("#{full_name}/#{usr_dir}")
     Dir.chdir("#{full_name}/#{usr_dir}") do
-      sh "curl http://production.cf.rubygems.org/rubygems/rubygems-#{rubygems}.tgz -s -o - | tar xzf -" if major_ruby == "1.8"
+      system "curl http://production.cf.rubygems.org/rubygems/rubygems-#{rubygems}.tgz -s -o - | tar xzf -" if major_ruby == "1.8"
     end
 
     # runtime ruby
