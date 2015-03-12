147dbe16648afec298d6786352497ee56e0bebd3
Mat Brown <mat@genius.com>
2015-03-12 13:28:58 -0400

    Fix

--- script/build_ree.rb
+++ script/build_ree.rb
@@ -15,10 +15,10 @@ def s3_upload(tmpdir, name)
   date_value = `date -R`.chomp
   content_type = "application/x-gzip"
   content_length = File.size("#{tmpdir}/#{name}.tgz")
-  string_to_sign = "PUT\n\n#{content_type}\n#{date_value}\n/#{s3_bucket_name}/#{platform}/#{name}"
+  string_to_sign = "PUT\n\n#{content_type}\n#{date_value}\n/#{s3_bucket_name}/#{platform}/#{name}.tgz"
   File.open('/tmp/string_to_sign', 'w') { |f| f << string_to_sign }
   signature = `cat /tmp/string_to_sign | openssl sha1 -hmac #{s3_secret} -binary | base64`.chomp
-  sh %(curl -i -XPUT -T #{tmpdir}/#{name}.tgz -H 'Host: #{s3_bucket_name}.s3.amazonaws.com' -H "Date: #{date_value}" -H "Content-Type: #{content_type}" -H "Authorization: AWS #{s3_key} #{signature}" https://#{s3_bucket_name}.s3.amazonaws.com/#{platform}/#{name}.tgz)
+  sh %(curl -i -XPUT -T #{tmpdir}/#{name}.tgz -H 'Host: #{s3_bucket_name}.s3.amazonaws.com' -H "Date: #{date_value}" -H "Content-Type: #{content_type}" -H "Authorization: AWS #{s3_key}:#{signature}" https://#{s3_bucket_name}.s3.amazonaws.com/#{platform}/#{name}.tgz)
 end
 
 def build_ree_command(name, output, prefix, usr_dir, tmpdir, rubygems = nil)
